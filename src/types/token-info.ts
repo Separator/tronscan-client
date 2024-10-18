a3d2a27692b093b94a971f5ddac6f282d6fd2e12
Максим Соловьев <mvs@kassa24.kz>
2024-10-18 23:43:52 +0500

    Move token info types to separate file

---
+++ src/types/token-info.ts
@@ -0,0 +1,107 @@
+interface TronScanTokenInfoCommon {
+  /**
+   * @description Token id (address or _ for native)
+   * @example '_'
+   */
+  tokenId: string;
+  /**
+   * @description Token name
+   * @example 'trx'
+   */
+  tokenName: string;
+  /**
+   * @description Token abbreviation
+   * @example 'trx'
+   */
+  tokenAbbr: string;
+  /**
+   * @description Number of decimal places
+   * @example 6
+   */
+  tokenDecimal: number;
+  /**
+   * @description Token can show
+   * @example 1
+   */
+  tokenCanShow: number;
+  /**
+   * @description Token logo url
+   * @example 'https://static.tronscan.org/production/logo/trx.png'
+   */
+  tokenLogo: string;
+  /**
+   * @description Token type string
+   * @example 'trc10'
+   */
+  tokenType: string;
+  /**
+   * @description Vip token status
+   * @example true
+   */
+  vip: boolean;
+}
+
+export interface TronScanTokenInfo extends TronScanTokenInfoCommon {
+  /**
+   * @description Token level
+   * @example '2'
+   */
+  tokenLevel: string;
+  /**
+   * @description Token issuer address
+   * @example 'THPvaUhoh2Qn2y9THCZML3H815hhFhn5YC'
+   */
+  issuerAddr?: string;
+}
+
+export interface TronScanTokenBalanceInfo extends TronScanTokenInfoCommon {
+  /**
+   * @description Amount in trx
+   * @example '12.744104'
+   */
+  amount: string;
+  /**
+   * @description Quantity
+   * @example '12.744104'
+   */
+  quantity?: string;
+  /**
+   * @description Token level
+   */
+  level?: string;
+  /**
+   * @description Token price in USD
+   * @example 0.123082017644144
+   */
+  tokenPriceInUsd?: number;
+  /**
+   * @description Token price in trx
+   * @example 1
+   */
+  tokenPriceInTrx: number;
+  /**
+   * @description Amount in USD
+   * @example 1.56857
+   */
+  amountInUsd?: number;
+  /**
+   * @description Balance in sun
+   * @example "12744104"
+   */
+  balance: string;
+  /**
+   * @description Number of token holders (not available in trx)
+   * @example 42_771_261
+   */
+  nrOfTokenHolders?: number;
+  /**
+   * @description Transfers count (not available in trx)
+   * @example 1_739_028_163
+   */
+  transferCount: number;
+  /**
+   * @description Project info
+   * @example ''
+   */
+  project?: string;
+}
